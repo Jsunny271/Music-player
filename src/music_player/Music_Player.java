3cafd9a72ded7f753961aa0b3b1a9d4ea4ba2816
jaatsunny <sunnymalikkiwana@gamil.com>
2019-04-16 23:00:03 +0530

    My very first commit

---
+++ src/music_player/Music_Player.java
@@ -0,0 +1,10 @@
+package music_player;
+
+public class Music_Player {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new MusicFrame().setVisible(true);        
+    }
+    
+}
